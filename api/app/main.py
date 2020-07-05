14ee30a962a07d5e1f1b13289d0a6b0715aa66ff
4tikhonov <4tikhonov@gmail.com>
2020-07-05 10:31:31 +0300

    Data API extended

--- api/app/main.py
+++ api/app/main.py
@@ -6,17 +6,20 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"CoronaWhy API": "v0.1"}
+    return {"CoronaWhy Data API": "v0.1"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: str = None):
+#    return {"item_id": item_id, "q": q}
 
-@app.get("/data/{item_id}")
+@app.get("/data/country/{item_id}")
+# http://api.apps.coronawhy.org/data/country/FRA
 def data_item(item_id: str, q: str = None):
     dataset = covid19(item_id, verbose = False)
-    #return { "data": dataset.to_json() }
     data = json.loads(dataset.to_json())
     return json.dumps(data, sort_keys=True, indent=4)
 
+@app.get("/data/pid/{item_id}")
+def data_persistent(item_id: str, q: str = None):
+    return {"PID": pid, "q": q}
 
