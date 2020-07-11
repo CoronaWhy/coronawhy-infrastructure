e3d0f0ed36c7ea90ef08c867f2d9d4930366ea51
4tikhonov <4tikhonov@gmail.com>
2020-07-12 02:05:43 +0300

    API extended with CORD-19 access to Mongo and Elastic

--- api/app/config.py
+++ api/app/config.py
@@ -1,2 +1,17 @@
+covid19datahub_title="COVID-19 Data Hub"
 covid19datahub_authors="COVID-19 Data Hub was initiated via the R package COVID19 developed by Emanuele Guidotti (University of Neuchâtel), leveraged by David Ardia (HEC Montréal) via the funding by IVADO, and enhanced and maintained by a growing open source community."
 covid19datahub_goal="The goal of COVID-19 Data Hub https://covid19datahub.io is to provide the research community with a unified data hub by collecting worldwide fine-grained case data, merged with exogenous variables helpful for a better understanding of COVID-19."
+covid19datahub_citation="Guidotti et al., (2020). COVID-19 Data Hub. Journal of Open Source Software, 5(51), 2376, https://doi.org/10.21105/joss.02376"
+covid19datahub_licence="We have invested a lot of time and effort in creating COVID-19 Data Hub, please: 1) cite Guidotti and Ardia (2020) in working papers and published papers that use it; 2) place the URL https://covid19datahub.io in a footnote to help others find COVID-19 Data Hub 3) you assume full risk for the use of COVID-19 Data Hub. We try our best to guarantee the data quality and consistency and the continuous filling of the Data Hub. However, it is free software and comes with ABSOLUTELY NO WARRANTY. Reliance on COVID-19 Data Hub for medical guidance or use of COVID-19 Data Hub in commerce is strictly prohibited."
+# Mongo
+mongohost="mongodb.coronawhy.org"
+mongouser='someadmin'
+mongopassword='somepassword'
+mongodatabase='cord19'
+cordversion='v22'
+# Elastic
+search_host = 'search.coronawhy.org'
+search_username = 'someelastic'
+search_password = 'mypassword'
+search_port = 80
+search_index_name = 'v22papers'
--- api/app/main.py
+++ api/app/main.py
@@ -1,19 +1,75 @@
+# -*- coding: utf-8 -*-
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from elasticsearch import helpers, Elasticsearch
 from covid19dh import covid19, cite
-from config import covid19datahub_goal, covid19datahub_authors
+from config import search_host, search_username, search_password, search_port, search_index_name, covid19datahub_title, covid19datahub_citation, covid19datahub_licence, covid19datahub_goal, covid19datahub_authors, mongohost, mongouser, mongopassword, mongodatabase, cordversion
+from pymongo import MongoClient
+from pyDataverse.api import Api, NativeApi
+import pandas as pd
 import json
 
-app = FastAPI()
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
 
-@app.get("/")
-def read_root():
-    return {"CoronaWhy Data API": "v0.1"}
+    openapi_schema = get_openapi(
+        title="CoronaWhy Data API",
+        description="CoronaWhy is globally distributed, volunteer-powered research organisation. We're trying to assist the medical community's ability to answer key questions related to COVID-19.",
+        version="0.1",
+        routes=app.routes,
+    )
+
+    openapi_schema['tags'] = tags_metadata
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+tags_metadata = [
+    {
+        "name": "country",
+        "externalDocs": {
+            "description": "Put this citation in working papers and published papers that use this dataset: %s" % covid19datahub_citation,
+            "authors": covid19datahub_authors,
+            "url": "https://covid19datahub.io",
+        },
+    },
+    {
+        "name": "cord",
+        "description": "Metadata by cord_id",
+        "externalDocs": {
+            "description": "CORD-19 collection access by cord_id",
+            "url": "https://api.apps.coronawhy.org/",
+        },
+    },
+    {
+        "name": "altmetrics",
+        "description": "Altmetrics by DOI or cord_id",
+        "externalDocs": {
+            "description": "CORD-19 papers Altmetrics",
+            "url": "https://api.apps.coronawhy.org/",
+        },
+    },
+    {
+        "name": "search",
+        "description": "CORD-19 search",
+        "externalDocs": {
+            "description": "CORD-19 papers search",
+            "url": "https://api.apps.coronawhy.org/",
+        },
+    }
+]
+
+app = FastAPI(
+    openapi_tags=tags_metadata
+)
+app.openapi = custom_openapi
 
 #@app.get("/items/{item_id}")
 #def read_item(item_id: int, q: str = None):
 #    return {"item_id": item_id, "q": q}
 
-@app.get("/country/{item_id}")
+@app.get("/country/{item_id}", tags=["country"])
 # http://api.apps.coronawhy.org/data/country/FRA
 def data_item(item_id: str, q: str = None):
     jsondataset = covid19(item_id, verbose = False)
@@ -21,12 +77,55 @@ def data_item(item_id: str, q: str = None):
     datapoints = json.loads(jsondataset.to_json(orient='records'))
     data['authors'] = str(covid19datahub_authors)
     data['goal'] = str(covid19datahub_goal)
+    data['licence'] = covid19datahub_licence
+    data['citation'] = covid19datahub_citation
+    data['downloads'] = 0
     data['data'] = datapoints
     data['citations'] = cite(jsondataset)
     #return json.dumps(data, sort_keys=True, indent=4)
     return data
 
-@app.get("/data_by_pid/{item_id}")
-def data_persistent(item_id: str, q: str = None):
-    return {"PID": pid, "q": q}
+@app.get("/cordsearch/", tags=["search"])
+def search_cord(fieldname: str = None, q: str = None):
+    if not fieldname:
+        fieldname = 'title'
+    # search_host, search_username, search_password, search_port, search_index_name
+    es = Elasticsearch("http://%s:%s@%s:%s/" % (search_username, search_password, search_host, search_port), Port=search_port)
+    if es is not None:
+        search_object = {'query': {'match': {fieldname: q}}}
+        res = es.search(index=search_index_name, body=json.dumps(search_object))
+        return res
+    return { 'q': 'no data' }
 
+@app.get("/cord/", tags=["cord"])
+def read_cord(cord_uid: str = None, pid: str = None):
+    client = MongoClient("mongodb://%s:%s@%s" % (mongouser, mongopassword, mongohost))
+    db = client.get_database(mongodatabase)
+    collection = db[cordversion]
+    PaperByCordID = {'cord_uid': {"$in": [cord_uid]}}
+    if pid:
+        PaperByCordID = {'doi': {"$in": [pid]}}
+    dbdata = collection.find(PaperByCordID)
+    df = pd.DataFrame(dbdata)
+    df = df[['cord_uid','title','authors','abstract','path']]
+    datapoints = df
+    data = {}
+    data['data'] = datapoints
+    return data
+
+@app.get("/altmetrics/", tags=["altmetrics"])
+def read_altmetrics(cord_uid: str = None, pid: str = None):
+    client = MongoClient("mongodb://%s:%s@%s" % (mongouser, mongopassword, mongohost))
+    altmetricsdb = client.get_database('altmetrics')
+    cordversion = 'v19'
+    altcollection = altmetricsdb[cordversion]
+    PaperByCordID = {'cord_uid': {"$in": [cord_uid]}}
+    if pid:
+        PaperByCordID = {'doi': {"$in": [pid]}}
+    data = {}
+    dbdata = altcollection.find(PaperByCordID)
+    metadata = []
+    for item in dbdata:
+        metadata.append(item['title'])
+    data['data'] = metadata
+    return data
